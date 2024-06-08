a56d604de7582893e4f590f1a05f4329ff1e9dc8
Huy Nguyen <huy.h.nguy@gmail.com>
2024-06-08 15:53:36 -0500

    create list of platforms

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -2,7 +2,14 @@ const Platform = require("../models/platform");
 const asyncHandler = require("express-async-handler");
 
 exports.platform_list = asyncHandler(async(req, res, next) => {
-    res.send("NOT IMPLEMENTED: platform list");
+    const allPlatforms = await Platform.find({}, "name description")
+        .sort({ name: 1 })
+        .exec();
+
+    res.render("platform_list", { 
+        title: "Platform List",
+        platform_list: allPlatforms,
+    })
 });
 
 exports.platform_detail = asyncHandler(async (req, res, next) => {
